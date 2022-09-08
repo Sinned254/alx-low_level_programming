6fc3a772d84b1828043b0ff444ebfce63b17ad16
Sinned254 <dennis1kosgei@gmail.com>
2022-09-08 11:59:37 -0700

    updated tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: zero always, printout a message
+ *
+ */	
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+

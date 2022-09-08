476dec2f7c37f890938cc3b1aa420ad0f893e3b6
Sinned254 <dennis1kosgei@gmail.com>
2022-09-08 12:20:23 -0700

    erro correction'

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,9 @@
 /**
  * main- entry to the program
  *
- * return: zero always, print size of types
+ * Description: use sizeof to determine types. 
+ *
+ * Return: zero always, print size of types
  */
 int main(void)
 {
@@ -11,6 +13,5 @@ int main(void)
 	printf("Size of a long int: %lu bytes\n(s)", sizeof(long int));
 	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu bytes(s)", sizeof(float));
-	
-	return(0);
+	return (0);
 }

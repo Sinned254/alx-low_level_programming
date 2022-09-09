42b4f83eb7ad7ebb3b74f572710efe315dedd6ab
Sinned254 <dennis1kosgei@gmail.com>
2022-09-09 05:47:03 -0700

    1 & 2 commit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/*
+ * main- entry to the program
+ *
+ * Description: Asses a random number, display if last digit is
+ * less than 6 or greater than 5 or is zero.
+ *
+ * Return: zere if successful
+ */
+int main(void)
+{
+	int n, ld;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	ld = n % 10;
+	if (ld <  6 && ld != 0)
+		printf("Last digit of %d is %d and is less than 6 and not zero\n", n, ld);
+	if (ld > 5)
+		printf("Last digit of %d is %d and is greater than 5 and not zero\n", n, ld);
+	if (ld == 0)
+		printf("Last digit of %d is 0 and is 0\n", n);
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Programs here are for practising on Cvariables if else while

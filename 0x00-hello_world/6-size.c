2c1618a7e53414094fa6b8935dc6b2866a76166f
Sinned254 <dennis1kosgei@gmail.com>
2022-09-08 23:59:39 -0700

    white space

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,16 +3,16 @@
 /**
  * main- entry to the program
  *
- * Description: use sizeof to determine types. 
+	 * Description: use sizeof to determine types 
  * Return: zero always, print size of types
  */
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu bytes(s)\n", sizeof(int));
-	printf("Size of a long int: %lu bytes\n(s)", sizeof(long int));
+	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("Size of a float: %lu bytes(s)", sizeof(float));
+	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
 
 	return (0);
 }

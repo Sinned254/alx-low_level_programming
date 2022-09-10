b7c6f06b59492aae4c1baac6d73ea29ae5cd09ff
Sinned254 <dennis1kosgei@gmail.com>
2022-09-10 11:57:50 -0700

    12 try

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+/**
+ * main- print numbers, entry to the prongram
+ *
+ * Description: prints all possible combination of double digits
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i, j, k, z;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+			while (j <= 9)
+			{
+				z = j;
+				k = i;
+					while (k <= 9)
+					{
+					while (z <= 9)
+					{
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + k);
+					putchar('0' + z);
+					if (i < 9 || j < 8 || k < 9 || z < 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					z++;
+					}
+					z = 0;
+					k++;
+					}
+					j++;
+			}
+			i++;
+	}
+	putchar('\n');
+	return (0);
+}
+

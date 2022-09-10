49b87cd569e865188c6abec884cd10567eebb8e9
Sinned254 <dennis1kosgei@gmail.com>
2022-09-10 04:48:50 -0700

    optional work done'

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main- entry to the program
+ *
+ * Description: print a quote of text
+ *
+ * Return: return 1 success
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	return (1);
+}
+

05da82f832a8722ffcc23b7ca86b2925034e37b6
Sinned254 <dennis1kosgei@gmail.com>
2022-09-13 06:14:21 -0700

    5 - 6 done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - prints + for positive num, - for negative
+ * number and 0 if 0
+ *
+ * @n: is number to be examined
+ *
+ * Return: 1 if positve 0 if zero if negative -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putcahr(0);
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints absolutes value of int
+ *@n: intiger to be examined
+ * Return: 0 success
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
+

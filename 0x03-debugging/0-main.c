cf862917d58eb8a4db36f773597d0c09994c5517
Sinned254 <dennis1kosgei@gmail.com>
2022-09-14 10:55:20 -0700

    debugging 0x03

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - test the function positive_or_negative
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (0);
+}

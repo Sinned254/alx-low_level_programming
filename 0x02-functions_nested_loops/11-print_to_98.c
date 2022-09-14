61ba6a58ace2c0f609fa1f9f274d997a7c379e8d
Sinned254 <dennis1kosgei@gmail.com>
2022-09-14 05:15:54 -0700

    11 erros space

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,14 +10,14 @@ void print_to_98(int n)
 	int i;
 
 	if (n == 98)
-		printf("%d ", 98);
+		printf("%d \n", 98);
 	else if (n <= 98)
 	{
 		for (i = n; i <= 97; i++)
 		{
 			printf("%d, ", i);
 			if (i == 97)
-				printf("%d ", 98);
+				printf("%d \n", 98);
 		}
 	}
 	else
@@ -26,7 +26,7 @@ void print_to_98(int n)
 		{
 			printf("%d, ", i);
 			if (i == 99)
-				printf("%d ", 98);
+				printf("%d \n", 98);
 		}
 	}
 }

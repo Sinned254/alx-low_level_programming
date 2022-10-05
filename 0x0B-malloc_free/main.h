9222d5cae070ab4370983c818b805b172bfe55b6
Sinned254 <dennis1kosgei@gmail.com>
2022-10-05 10:53:40 -0700

    1 try

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,7 @@
 #include <stddef.h>
 char *create_array(unsigned int size, char c);
 #include <stdlib.h>
-int _strlen(char *s);
+unsigned int _strlen(char *s);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);

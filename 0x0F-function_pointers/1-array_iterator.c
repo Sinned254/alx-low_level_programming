907a6d700ad972b1f30a34a1aa92aa407a20683c
Sinned254 <dennis1kosgei@gmail.com>
2022-10-13 11:52:21 -0700

    betty issue

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,10 +8,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i = 0;
+
 	if (array == NULL || action == NULL)
 		return;
-
-	size_t i = 0;
 
 	while (i < size)
 	{

816b33619883323e93bda7e81dde41b8f36f816e
Sinned254 <dennis1kosgei@gmail.com>
2022-10-24 05:07:57 -0700

    changes in print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,15 +8,15 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-
 	i = 0;
 
 	while (h != NULL)
 	{
 		i++;
-		printf("[%d} %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d} %s\n", h->len, h->str);
 		h = h->next;
 	}
 	return (i);

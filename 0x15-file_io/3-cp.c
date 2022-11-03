e94457f9dc67c85dee8bd567acca321f198e373f
Sinned254 <dennis1kosgei@gmail.com>
2022-11-03 11:44:36 -0700

    3 edit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,13 +17,11 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
-
 	if (fd1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	if (fd2 == -1)
@@ -43,14 +41,9 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if (close(fd1) == -1)
+	if (close(fd1) == -1 || close(fd2) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
-	}
-	if (close(fd2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 		exit(100);
 	}
 	return (0);

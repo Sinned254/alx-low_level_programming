d7e9a5df991d495e9827d725ecfb845f7638e200
Sinned254 <dennis1kosgei@gmail.com>
2022-11-03 11:33:24 -0700

    add 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include "main.h"
+/**
+ * main - copy content of file to another
+ * @argc: arguments counts
+ * @argv: arguments vector
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2;
+	char buffer[1024];
+	ssize_t copybytes;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	copybytes = 1;
+	while (copybytes)
+	{
+		copybytes = read(fd1, buffer, 1024);
+
+		if (write(fd2, buffer, copybytes) != copybytes)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <stdarg.h>
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);

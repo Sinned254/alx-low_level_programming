8fe0fcaf68288dabbc47ddb0d802f333519edd16
Sinned254 <dennis1kosgei@gmail.com>
2022-12-13 10:40:30 -0800

    some errror

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gcc -Wall -fPIC -c *.c
-gcc -shared -W1,-soname,liball.so -o liball.so *.o
+gcc -shared -Wl,-soname,liball.so -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH

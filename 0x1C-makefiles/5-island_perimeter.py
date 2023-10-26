421bd0a9642044d18b061cb6bd1a1f19b1a14db8
Denis Kosgei <dennis1kosgei@gmail.com>
2023-10-25 22:41:24 -0700

    resolve errors

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
+"""Island perimeter funtion"""
+
 
 def island_perimeter(grid):
     """
     Calculate the perimeter of an island represented by a grid.
 
     Args:
-    grid (list of list of int): A 2D grid where 0 represents water and 1 represents land.
+    grid (list of list of int): A 2D grid where 0
+    represents water and 1 represents land.
 
     Returns:
     int: The perimeter of the island.
 
     Constraints:
     - The grid is rectangular, with width and height not exceeding 100.
-    - The grid is completely surrounded by water, and there is one island (or nothing).
-    - The island doesn't have "lakes" (water inside that isn't connected to the water around the island).
+    - The grid is completely surrounded by water,
+    and there is one island (or nothing).
+    - The island doesn't have "lakes"
+    (water inside that isn't connected to the water around the island).
     - Grid cells are connected horizontally/vertically (not diagonally).
 
     """
@@ -36,8 +41,7 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                perimeter += 4  # Start with 4, accounting for potential neighbors
-
+                perimeter += 4
                 # Subtract the number of adjacent land cells
                 perimeter -= count_neighbors(i, j)
 
